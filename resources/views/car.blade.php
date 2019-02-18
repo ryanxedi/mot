67e9deb12ff9d1168ae056b98c388fda33347383
XEDI-RYAN\Ryan <ryan@xedi.com>
2019-02-18 12:44:13 +0000

    jQuery no spaces added

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -8,116 +8,33 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
 
         <!-- Styles -->
         <script src="jquery-1.8.3.min.js"></script>
 
         <script>
-        jQuery(function($){
-            $('#numberPlate').keyup(function(e){
-                if (e.which === 32) {
-                    alert('No space are allowed in usernames');
+            jQuery(function($){
+                $('#numberPlate').keyup(function(e){
+                    if (e.which === 32) {
+                        alert('No space are allowed in usernames');
+                        var str = $(this).val();
+                        str = str.replace(/\s/g,'');
+                        $(this).val(str);            
+                    }
+                }).blur(function() {
                     var str = $(this).val();
                     str = str.replace(/\s/g,'');
                     $(this).val(str);            
-                }
-            }).blur(function() {
-                var str = $(this).val();
-                str = str.replace(/\s/g,'');
-                $(this).val(str);            
+                });
             });
-        });
         </script>
-
-        <style>
-        @font-face {
-              font-family: 'UKNumberPlate';
-              src: url('fonts/UKNumberPlate.ttf');
-            }
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .numberplate {
-                background:yellow; 
-                border-radius: 5px; 
-                font-family:UKNumberPlate; 
-                font-size:170px; 
-                text-align: center; 
-                width:740px; 
-                height:165px;
-                border: 3px solid #666;
-                color:#333;
-                padding-bottom: 50px;
-            }
-        </style>
 
     </head>
     <body>
 
 
         <div style="padding:60px 0" class="flex-center">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 @foreach($carsInformation as $carInformation)
                     <div class="numberplate">
@@ -162,9 +79,7 @@
                             @endforeach
                         </div>
                      @endforeach
-<!--                     <?php dd($motTest); ?> -->
                 @endforeach
-
         </div>
     </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,112 +8,30 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link href="css/style.css" rel="stylesheet">
         <!-- Styles -->
-        <script src="jquery-1.8.3.min.js"></script>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <script>
-        jQuery(function($){
-            $('#registration').keyup(function(e){
-                if (e.which === 32) {
-                    alert('No space are allowed in usernames');
-                    var str = $(this).val();
-                    str = str.replace(/\s/g,'');
-                    $(this).val(str);            
-                }
-            }).blur(function() {
-                var str = $(this).val();
-                str = str.replace(/\s/g,'');
-                $(this).val(str);            
+            $(function() {
+                $('#registration').on('keypress', function(e) {
+                    if (e.which == 32)
+                        return false;
+                });
             });
-        });
         </script>
 
         <style>
-        @font-face {
-              font-family: 'UKNumberPlate';
-              src: url('fonts/UKNumberPlate.ttf');
-            }
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
             .numberplate {
-                background:yellow; 
-                border-radius: 5px; 
-                font-family:UKNumberPlate; 
-                font-size:170px; 
-                text-align: center; 
                 width:750px; 
                 height:180px;
-                border: 3px solid #666;
+                padding-top: 50px
             }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">Enter your registration number</div>
                 <form method="POST" action="car">
