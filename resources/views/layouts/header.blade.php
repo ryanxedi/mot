b93ee08547d00df33444119e07dbe00edba5acbc
infusion2k7 <ryan@xedi.com>
2019-02-19 23:13:54 +0000

    Updated background and added a favicon

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Enter your registration number to get a full MOT history of your car">
+
+    <link rel="icon" type="image/png" href="favicon.png">
+
+    <title>Check My MOT</title>
+ 
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sticky-footer-navbar/">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+ 
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script>
+        $(function() {
+            $('#registration').on('keypress', function(e) {
+                if (e.which == 32)
+                    return false;
+            });
+        });
+    </script>
+  </head>
