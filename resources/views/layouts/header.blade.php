1a3131a87bcb76683aa04233ec63979a4b783096
infusion2k7 <ryan@xedi.com>
2019-02-19 23:39:15 +0000

    Added more OG metadata

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Enter your registration number to get a full MOT history of your car">
 
+    <meta property="og:url" content="https://checkmymot.uk">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Check My MOT - Your MOT History Without the Fuss">
     <meta property="og:image" content="https://checkmymot.uk/og-image.png">
     <meta property="og:description" content="Enter your registration number to get a full MOT history of your car">
 
+
     <link rel="icon" type="image/png" href="favicon.png">
 
-    <title>Check My MOT</title>
+    <title>Check My MOT - Your MOT History Without the Fuss</title>
  
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sticky-footer-navbar/">
+    <link rel="canonical" href="https://checkmymot.uk">
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
  
