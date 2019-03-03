23321cbe28e239fbea987179718964c8fdbdb108
infusion2k7 <ryan@xedi.com>
2019-03-03 21:15:06 +0000

    Reorganised css delivery for nicer response times

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,9 +19,8 @@
     <title>Check My MOT - Your MOT History Without the Fuss</title>
  
     <link rel="canonical" href="https://checkmymot.uk">
-    <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- 
+    <link href="css/style.css" rel="stylesheet"> 
  
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script>
